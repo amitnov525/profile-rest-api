2c8a841d47cd46e6ab5f38b07cbfe06b3357c1eb
Amit <amit170111nov@gmail.com>
2022-08-13 19:53:52 +0530

    Added helloapi for demonstration

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import  APIView
+from rest_framework.response import Response
+
+class HelloAPIView(APIView):
+    """TEST API VIEW"""
+    def get(self,request,format=None):
+        """Returns a list of API features"""
+
+        api_view=[
+            'This is a demonstration of API View',
+        ]
+        return Response({'message':'This is an view','api_view':api_view})
+    
